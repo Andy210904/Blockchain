1758ce702b642fb58edd4847c8dde69bb8628ee0
Andy210904 <adityadhane553@gmail.com>
2024-03-13 15:48:36 +0530

    updated

---
+++ bank/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Content.css'
+function Content() {
+  return (
+    <div className='content'>
+       <h2>Bank Details</h2>
+       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente quidem qui temporibus deserunt cum id nulla consequatur expedita, culpnulla totam.</div>
+    </div>
+  )
+}
+
+export default Content
---
+++ bank/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css'
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar-items'>My DashBoard</div>
+        <div className='sidebar-items'>TOTM Links</div>
+        <div className='sidebar-items'>Daily Summary</div>
+        <div className='sidebar-items'>Bank Details</div>
+    </div>
+  )
+}
+
+export default Sidebar
